e5fe8610fb37448701fe0de13f4273cfe0981de0
Moulik <moulikbudhiraja@gmail.com>
2023-10-10 00:10:19 -0400

    fix: waitlist1 deadline

--- src/constants/eventDates.ts
+++ src/constants/eventDates.ts
@@ -4,5 +4,5 @@ export const RSVPDeadline = new Date(
 );
 
 export const RSVPWaitlist1Deadline = new Date(
-  "Tue Oct 10 2023 00:00:00 GMT-0400 (Eastern Daylight Saving Time)"
+  "Tue Oct 10 2023 23:59:59 GMT-0400 (Eastern Daylight Saving Time)"
 );
